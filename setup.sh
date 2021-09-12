0c58536b1acdda023e5e5369225ea4bfad3adbb1
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2021-09-12 18:38:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://reedem.axlinesid.com:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
+IZIN=$( curl https://doragcp.axlinesid.online:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
