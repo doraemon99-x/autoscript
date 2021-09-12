888a4095a3c891e2f676d4e202630352b55e0381
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2021-09-12 17:41:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl http://ipinfo.io/ip | grep $MYIP )
+IZIN=$( curl 35.247.141.103 | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
